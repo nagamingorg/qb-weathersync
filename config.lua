d8624044bf04a844c268e89b9f20f45fa8b991a8
Walrus <thegloriouswalrus.0001@gmail.com>
2024-06-05 21:48:11 -0400

    Latest official weathersync repo update

--- config.lua
+++ config.lua
@@ -3,13 +3,14 @@ Config.DynamicWeather   = true -- Set this to false if you don't want the weathe
 
 -- On server start
 Config.StartWeather     = 'EXTRASUNNY' -- Default weather                       default: 'EXTRASUNNY'
-Config.BaseTime         = 8 -- Time                                             default: 8
+Config.BaseTime         = 12 -- Time                                             default: 8
 Config.TimeOffset       = 0 -- Time offset                                      default: 0
-Config.FreezeTime       = false -- freeze time                                  default: false
+Config.FreezeTime       = true -- freeze time                                  default: false
 Config.Blackout         = false -- Set blackout                                 default: false
 Config.BlackoutVehicle  = false -- Set blackout affects vehicles                default: false
 Config.NewWeatherTimer  = 20 -- Time (in minutes) between each weather change   default: 10
 Config.Disabled         = false -- Set weather disabled                         default: false
+Config.RealTimeSync     = false -- Activate realtime synchronization            default: false
 
 Config.AvailableWeatherTypes = { -- DON'T TOUCH EXCEPT IF YOU KNOW WHAT YOU ARE DOING
     'EXTRASUNNY',
